56fab8a4bde27a0a43c7dd8b56ba8ec741536a51
itsandreramon <thielea@th-brandenburg.de>
2024-01-06 22:45:08 +0100

    add

---
+++ app/components/Navigation.js
@@ -0,0 +1,8 @@
+export default function Navigation({ children }) {
+  return (
+    <div className="flex flex-col">
+      <p className="font-mono font-bold">André Thiele</p>
+      <p className="font-mono">Navigation</p>
+    </div>
+  )
+}
