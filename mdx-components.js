92526316a43eb38229a78f8151509405bdedc1b0
itsandreramon <thielea@th-brandenburg.de>
2024-01-07 22:45:08 +0100

    update

--- mdx-components.js
+++ mdx-components.js
@@ -1,10 +1,11 @@
 export function useMDXComponents(components) {
   return {
-    p: ({ children }) => <p className="pb-4 text-slate-400">{children}</p>,
-    h1: ({ children }) => <h1 className="font-bold text-4xl pb-4">{children}</h1>,
-    h2: ({ children }) => <h2 className="font-bold text-3xl pb-2">{children}</h2>,
-    h3: ({ children }) => <h3 className="font-bold text-2xl pb-4">{children}</h3>,
-    h4: ({ children }) => <h4 className="font-bold text-2xl pb-4">{children}</h4>,
+    p: ({ children }) => <p className="pb-8 text-slate-400 text-justify">{children}</p>,
+    h1: ({ children }) => <h1 className="font-bold py-4">{children}</h1>,
+    h2: ({ children }) => <h2 className="font-bold py-4">{children}</h2>,
+    h3: ({ children }) => <h3 className="font-bold py-4">{children}</h3>,
+    h4: ({ children }) => <h4 className="font-bold py-4">{children}</h4>,
+    ul: ({ children }) => <ul style={{ listStyleType: `decimal`, listStylePosition: `inside` }}>{children}</ul>,
     ...components,
   }
 }
